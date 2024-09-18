dc95d48c5dedd93c490fad1473984cf44b9ffa9f
sschwartz <sschwartz@OMSOBL311Schwartz.stevenson.edu>
2024-09-18 17:41:53 -0400

    Trying this from the project right click on project to team to share project to create.

---
+++ src/playGround/pg1.java
@@ -0,0 +1,16 @@
+package playGround;
+import static java.lang.System.*;
+
+import java.util.Scanner;
+public class pg1 {
+	public static void main(String[] args) {
+		// Sums all numbers 1-100
+		// TODO Auto-generated method stub
+		int sumNumbers = 0;
+		for (int i = 1; i <= 100; i++)
+			sumNumbers += i;
+		System.out.println("The sum of numbers from 1 - 100 is: " +sumNumbers);
+		System. out.println("Right click on project to team, share project");
+	}
+
+}
